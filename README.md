392e1b1a04b2eefd5300021824a8f5306caa653c
Alex Olar <olaralex666@gmail.com>
2024-09-24 13:58:20 +0200

    added local setup for jellyfin media server and its automation

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 docker run authelia/authelia:latest authelia crypto hash generate argon2 --password 'YOUR PASSWORD HERE'
 ```
 
+## Notes
+
+1) Mounted my hard drive to `/mnt/media` and created the folders `Movies`, `TV`, `Downloads` so that the mounted volumes are `/mnt/media/Movies`, `/mnt/media/TV`, `/mnt/media/Downloads`.
+
+2) docker compose up -d --build # To build the images and start the containers.
+
+3) docker logs -f traefik
+
---
+++ local-compose/create_dirs.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import yaml
+
+def create_dirs(volume_path):
+    local_path = volume_path.split(':')[0]
+    Path(local_path).mkdir(parents=True, exist_ok=True)
+    
+if __name__ == '__main__':
+    compose_file = './compose.yaml'
+    
+    with open(compose_file) as file:
+        compose = yaml.load(file, Loader=yaml.FullLoader)
+        
+    for service in compose['services']:
+        if 'volumes' in compose['services'][service]:
+            for volume in compose['services'][service]['volumes']:
+                create_dirs(volume)
+                
+    
