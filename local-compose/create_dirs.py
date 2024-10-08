b0267f80be43a162fec466e72a639c18cfa62663
Alex Olar <olaralex666@gmail.com>
2024-10-08 17:44:43 +0200

    nginx reverse proxy with duckdns is finally working

--- local-compose/create_dirs.py
+++ local-compose/create_dirs.py
@@ -3,6 +3,12 @@ import yaml
 
 def create_dirs(volume_path):
     local_path = volume_path.split(':')[0]
+    if Path(local_path).exists():
+        return
+    
+    if '$' in local_path:
+        return
+    
     Path(local_path).mkdir(parents=True, exist_ok=True)
     
 if __name__ == '__main__':
